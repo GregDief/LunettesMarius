c9e464dca35deefccd2d7420dbd3f30aa19e001c
Grégoire DIEF <gregoiredief@gmail.com>
2019-02-07 16:10:59 +0100

    display attempt

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
+
   def home
+    @client = Airtable::Client.new("keycKFESm2GX7qyoY")
+    @table = @client.table("tbldR3vDVOygwvJ4Q", "lunettes marius")
+    @records = @table.all
+
+  end
+
+  def table
+    @table = @client.table("appfipsvymTGF8wP5", "lunettes marius")
   end
 end
